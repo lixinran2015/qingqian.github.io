f7991234b070cc02792d54a2fe1030fae26c50a6
lixinran <lixinran2010@gmail.com>
2016-04-25 18:56:50 +0800

    解决冲突后提交代码

---
+++ bigData/src/pers/bigData/action/web/UserAction.java
@@ -0,0 +1,31 @@
+package pers.bigData.action.web;
+
+import java.util.Collection;
+
+import pers.bigData.dao.Dao;
+import pers.bigData.vo.UserModel;
+
+public class UserAction {
+    Dao dao;
+
+    public void setDao(Dao dao) {
+        this.dao = dao;
+    }
+
+    public void toList() {
+        System.out.println("toList===" + dao);
+        UserModel um = new UserModel();
+        um.setId("id222");
+        um.setId("i333");
+        um.setId("local");
+        um.setId("remote");
+        um.setName("namewww");
+        dao.create(um);
+        // dao.update(um);
+        // dao.delete("id111");
+        Collection col = dao.getAll();
+        System.out.println(col);
+    }
+    
+   
+}

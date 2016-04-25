6cf3a0272742544b09d02acb76bfe72d18ff9ad0
lixinran2015 <lixinran2010@gmail.com>
2016-04-25 18:36:13 +0800

    Update UserAction.java

--- bigData/src/pers/bigData/action/web/UserAction.java
+++ bigData/src/pers/bigData/action/web/UserAction.java
@@ -17,7 +17,6 @@ public class UserAction {
         UserModel um = new UserModel();
         um.setId("id222");
         um.setId("i333");
-        um.setId("remote");//bylixr
         um.setId("local"); //bylixinran
         um.setName("namewww");
         dao.create(um);

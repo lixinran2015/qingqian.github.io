7b9a95ca4605a49277476719603cbe4ba23c18d0
lixinran <lixinran2010@gmail.com>
2016-04-26 18:16:54 +0800

    java 学习第五课

---
+++ Hello_New_World/src/pers/qingqian/study/five/HelloWorldFive.java
@@ -0,0 +1,22 @@
+/** 
+ * Project Name:Hello_New_World 
+ * File Name:HelloWorldFive.java 
+ * Package Name:pers.qingqian.study.five 
+ * Date:2016年4月26日下午6:16:16 
+ * Copyright (c) 2016, qingqian All Rights Reserved. 
+ * 
+ */  
+package pers.qingqian.study.five;  
+/** 
+ * ClassName:HelloWorldFive <br/> 
+ * Function: TODO ADD FUNCTION. <br/> 
+ * Reason:   TODO ADD REASON. <br/> 
+ * Date:     2016年4月26日 下午6:16:16 <br/> 
+ * @author   qingqian
+ * @version   
+ * @see       
+ */
+public class HelloWorldFive {
+
+}
+ 
